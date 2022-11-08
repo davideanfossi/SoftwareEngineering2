a22c018c344e0db54bb41851824dc5999e4edad0
DIEGO ZANFARIDNO s295555 <s29555@studenti.polito.it>
2022-11-08 11:36:13 +0100

    Revert "S202216HT-49 | Basic implementation of registration with 1 factor authentication"
    This reverts commit 47912b6c6d233a081583e71b535c2e2b7e728aaf.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 'use strict';
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const session = require('express-session');
+
 const DbManager = require("./database/dbManager");
 const hikeController = require('./controllers/hikeController');
-const authRoutes = require('./controllers/authController');
+
 // init express
 const app = express();
 const port = 3001;
@@ -31,8 +32,6 @@ dbManager.openConnection()
 
 /********* APIs *********/
 app.use("/api", hikeController);
-app.use("/api", authRoutes);
-
 
 // activate the server
 app.listen(port, () => console.log(`Server started at http://localhost:${port}.`));
