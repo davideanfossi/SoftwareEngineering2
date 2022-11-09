d0f6969dbf9068e22552f165ffe4b19dcecdb865
LucaBaratta98 <lucabt27@gmail.com>
2022-11-09 17:16:04 +0100

    started with the login layout

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,10 +1,45 @@
-import { useState } from "react"
+import { useState, React } from 'react';
+import { Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Outlet } from 'react-router-dom';
+import { Register } from './Register';
 
 function LoginForm() {
     
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [name, setName] = useState('');
+
+    return (
+        <>
+        <h1>Hike Tracker</h1>
+        <h3>Please insert your email and password:</h3>
+        <form>
+            <label>email:
+                <input type="text" />
+            </label>
+        </form>
+        <form>
+            <label>password:
+                <input type="password" name="password" />
+            </label>
+        </form>
+        If you are not already registered:
+        <nav>
+            <li>
+                <Link to='/register'>click here</Link>
+            </li>
+        </nav>
+        <Outlet />
+        <Router>
+            <Route path="register" element={<Register />} />
+        </Router>
+        <Button color='yellow'>
+            Login
+        </Button>
+        </>
+    );
 
 }
 
+export { LoginForm };
 
---
+++ client/src/Components/Register.js
@@ -0,0 +1,9 @@
+function Register() {
+    return(
+        <>
+        <h1>Hike Tracker</h1>
+        </>
+    );
+}
+
+export { Register };
