d8852829d58d965a0b0359b9dd19e9382168d21e
AngeloMicheleDelGuercio <s289572@studenti.polito.it>
2022-11-12 13:15:10 +0100

    S202216HT-51 loginService, fix

--- server/dtos/userDTO.js
+++ server/dtos/userDTO.js
@@ -1,9 +1,19 @@
 'use strict'
 
 class userDTO {
-    constructor(id, user) {
+    constructor(id, username, email, role, password, salt, name, surname, phoneNumber, isVerified, token, tokenExpires) {
        this.id = id;
-       this.user = user;
+       this.username = username;
+       this.email = email;
+       this.role = role;
+       this.password = password;
+       this.salt = salt;
+       this.name = name;
+       this.surname = surname;
+       this.phoneNumber = phoneNumber;
+       this.isVerified = isVerified;
+       this.token = token;
+       this.tokenExpires = tokenExpires;
     }
 }
 
