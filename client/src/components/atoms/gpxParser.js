32afed402e8269cde38b66b8facda6116fa6343b
Alessandro Zamparutti s301132 <s301132@studenti.polito.it>
2022-12-07 11:07:29 +0100

    fix gpx parser compute ascent and add test data

--- client/src/components/atoms/gpxParser.js
+++ client/src/components/atoms/gpxParser.js
@@ -6,15 +6,15 @@ const parseGpx = (file) => {
     try {
         const gpxFile = new DOMParser().parseFromString(file);
         const geoJson = togeojson.gpx(gpxFile);
-        const desc = geoJson.features[0].properties.desc;
-        const name = geoJson.features[0].properties.name;
+        const desc = geoJson.features[0].properties.desc ? geoJson.features[0].properties.desc : "";
+        const name = geoJson.features[0].properties.name ? geoJson.features[0].properties.name : "";
 
         const gpx = new gpxParser();
         gpx.parse(file);
         const start = {"lat": gpx.tracks[0].points[0].lat, "lon": gpx.tracks[0].points[0].lon, "ele": gpx.tracks[0].points[0].ele};
         const end = {"lat": gpx.tracks[0].points[gpx.tracks[0].points.length - 1].lat, "lon": gpx.tracks[0].points[gpx.tracks[0].points.length - 1].lon, "ele": gpx.tracks[0].points[gpx.tracks[0].points.length - 1].ele};
 		const length = Math.round(parseFloat(gpx.tracks[0].distance.total).toFixed(2));
-		const ascent = Math.round(parseFloat(gpx.tracks[0].elevation.max - gpx.tracks[0].elevation.min).toFixed(2));
+		const ascent = Math.round(parseFloat(gpx.tracks[0].elevation.pos.toFixed(2)));
         return {start, end, name, desc, length, ascent};
     } catch(err) {
         console.log(err);
