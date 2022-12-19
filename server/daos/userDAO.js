c9525d94f7402799015155925e0994ae9fda6f04
Alessandro Zamparutti s301132 <s301132@studenti.polito.it>
2022-12-19 09:50:34 +0100

    add methods for user dao

--- server/daos/userDAO.js
+++ server/daos/userDAO.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const User = require('../models/userModel');
+const RecordedHike = require("../models/recordedHike");
 const crypto = require("crypto");
 
 class UserDAO {
@@ -13,20 +14,20 @@ class UserDAO {
 
     async getUserByEmail(email) {
         const query = "SELECT * FROM User WHERE email = ?";
-            const result = await this.dbManager.get(query, [email], true);
-            return new User(result.id, result.username);
+        const result = await this.dbManager.get(query, [email], true);
+        return new User(result.id, result.username);
     }
 
     async getUserById(userId) {
         const query = "SELECT * FROM User WHERE id = ?";
-            const result = await this.dbManager.get(query, [userId], true);
-            return new User(result.id, result.username, result.email, result.role);
+        const result = await this.dbManager.get(query, [userId], true);
+        return new User(result.id, result.username, result.email, result.role);
     }
 
     async getUserByCredentials(username, email, role) {
         const query = "SELECT * FROM User WHERE email = ? AND username = ? AND role = ?";
-            const result = await this.dbManager.get(query, [email, username, role]);
-            return new User(result.id, result.username, result.email, result.role);
+        const result = await this.dbManager.get(query, [email, username, role]);
+        return new User(result.id, result.username, result.email, result.role);
     }
 
     async loginUser(email, password) {
@@ -34,9 +35,9 @@ class UserDAO {
         const user = await this.dbManager.get(sql, [email], true);
         if (user === undefined) {
             // user does not exist
-            throw {msg:"Incorrect username and/or password. Error code: ",err: 401};
+            throw { msg: "Incorrect username and/or password. Error code: ", err: 401 };
         }
-        if(user.isVerified === 0)
+        if (user.isVerified === 0)
             throw { msg: "Account must be verified. Error code: ", err: 401 };
         const login = await verifyPassword(
             user.password,
@@ -45,11 +46,36 @@ class UserDAO {
         );
         if (!login)
             throw {
-              msg: "Incorrect username and/or password. Error code: ",
-              err: 401,
+                msg: "Incorrect username and/or password. Error code: ",
+                err: 401,
             };
-        return {"id": user.id, "username": user.username, "email": user.email, "role": user.role};
+        return { "id": user.id, "username": user.username, "email": user.email, "role": user.role };
     }
+
+    async insertRecordedHike(recordedHike) {
+        const sql = "INSERT INTO RecordedHike(hikeId, userId, startDateTime, endDateTime) VALUES(?, ?, ?, ?)";
+        const res = await this.dbManager.query(sql, [recordedHike.hikeId, recordedHike.userId, recordedHike.startDateTime, recordedHike.endDateTime]);
+        return res;
+    }
+
+    async updateRecordedHike(recordedHike) {
+        const sql = "UPDATE RecordedHike SET startDateTime = ?, endDateTime = ? WHERE hikeId = ? AND userId = ?";
+        const res = await this.dbManager.query(sql, [recordedHike.startDateTime, recordedHike.endDateTime, recordedHike.hikeId, recordedHike.userId]);
+        return res;
+    }
+
+    async getRecordedHike(hikeId, userId) {
+        const sql = "SELECT * FROM RecordedHike WHERE hikeId = ? AND userId = ?";
+        const res = await this.dbManager.get(sql, [hikeId, userId], true);
+        return new RecordedHike(res.id, res.hikeId, res.userId, res.startDateTime, res.endDateTime);
+    }
+
+    async getRecordedHikeById(recordedHikeId) {
+        const sql = "SELECT * FROM RecordedHike WHERE id = ?";
+        const res = await this.dbManager.get(sql, [recordedHikeId], true);
+        return new RecordedHike(res.id, res.hikeId, res.userId, res.startDateTime, res.endDateTime);;
+    }
+
 
 }
 
---
+++ server/models/recordedHike.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class RecordedHike{
+
+    constructor(id, hikeId, userId, startDateTime, endDateTime){
+        this.id = id;
+        this.hikeId = hikeId;
+        this.userId = userId;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+    }
+}
+
+module.exports = RecordedHike;
